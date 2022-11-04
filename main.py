36c92f6e605fca3402dfe8c7e6443a904a74dc4b
Anthony Massaad <62800170+tony1bally@users.noreply.github.com>
2022-11-04 15:33:00 -0400

    added scaled and fps

--- main.py
+++ main.py
@@ -11,11 +11,22 @@ class Game:
         """Game initialization and setup
         """
         pygame.init()
-        self.display = pygame.display.set_mode((GAME_WIDTH + 1, GAME_HEIGHT + 1))
+        info = pygame.display.Info()
+        w = info.current_w
+        h = info.current_h
+
+        self.display = pygame.display.set_mode((w, h), pygame.FULLSCREEN)
         pygame.display.set_caption('Legend of Dylan')
+        self.basic_font = pygame.font.SysFont("Arial", 20)
         self.clock = pygame.time.Clock()
         self.world = World(starting_terrain)
     
+    def draw_fps(self):
+        fps = f'FPS: {int(self.clock.get_fps())}'
+        fps_text = self.basic_font.render(fps, 1, Color.BLACK.value)
+        self.display.blit(fps_text, (10, 0))
+        return fps_text
+
     def run(self) -> None:
         """
             Main game loop
@@ -24,14 +35,21 @@ class Game:
         while True: 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    Log.info("Exiting game")
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        Log.info("Exiting game")
+                        pygame.quit()
+                        sys.exit()
             
             # delta time needed to make everything frame rate independent 
             # so, regardless of the fps, the game will be executed at the same speed.
             delta_time = self.clock.tick() / 1000
             # print(delta_time)
             self.world.run(delta_time)
+            self.draw_fps()
             pygame.display.update()
 
 
